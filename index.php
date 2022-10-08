802167392c71ad901564525075d53a7f62993f0d
bostickt201 <96546828+bostickt201@users.noreply.github.com>
2022-10-08 19:13:24 -0400

    Update index.php
    testing ssl

--- index.php
+++ index.php
@@ -12,8 +12,13 @@
 echo "php is working";
 echo "php is working good";
 
-$con = mysqli_init(); mysqli_real_connect($conn, "teambuilder.mysql.database.azure.com", "Team10admin", "hLGV3FisTb7P39z", "teambuilder", 3306, MYSQLI_CLIENT_SSL);
-echo $con;
+
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL, "/home/site/certs/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_real_connect($conn, 'teambuilder.mysql.database.azure.com', 'Team10admin', 'hLGV3FisTb7P39z', 'teambuilder', 3306, MYSQLI_CLIENT_SSL);
+if (mysqli_connect_errno($conn)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
 ?>
 
 </body>
