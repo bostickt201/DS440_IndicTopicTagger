0fdfea1767d39e8cfde9233ff68d437dddd31b71
bostickt201 <96546828+bostickt201@users.noreply.github.com>
2022-10-08 19:16:43 -0400

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,9 @@ $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "/home/site/certs/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, 'teambuilder.mysql.database.azure.com', 'Team10admin', 'hLGV3FisTb7P39z', 'teambuilder', 3306, MYSQLI_CLIENT_SSL);
 if (mysqli_connect_errno($conn)) {
-die('Failed to connect to MySQL: '.mysqli_connect_error());
+ die('Failed to connect to MySQL: '.mysqli_connect_error());
+}else{
+ echo "mysql is working good";
 }
 ?>
 
