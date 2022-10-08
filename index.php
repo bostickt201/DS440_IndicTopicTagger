d5e7fba6da2a6e79cc61efc6667651fba57e20f1
bostickt201 <96546828+bostickt201@users.noreply.github.com>
2022-10-08 15:13:46 -0400

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,9 @@
 <?php
 echo "php is working";
 echo "php is working good";
+
+$con = mysqli_init(); mysqli_real_connect($conn, "teambuilder.mysql.database.azure.com", "Team10admin", "hLGV3FisTb7P39z", "teambuilder", 3306, MYSQLI_CLIENT_SSL);
+echo $con;
 ?>
 
 </body>
