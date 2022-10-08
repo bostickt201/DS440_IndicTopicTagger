3f19b2a4509f40debb2ea703b0e94280fd6ced20
bostickt201 <96546828+bostickt201@users.noreply.github.com>
2022-10-08 19:14:06 -0400

    Update index.php

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
    
 <?php
 echo "php is working";
+</br>
 echo "php is working good";
-
-
+</br>
 $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "/home/site/certs/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, 'teambuilder.mysql.database.azure.com', 'Team10admin', 'hLGV3FisTb7P39z', 'teambuilder', 3306, MYSQLI_CLIENT_SSL);
