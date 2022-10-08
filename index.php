8115e60aca5cd00d84db3ae661a80337a0dc9a9c
bostickt201 <96546828+bostickt201@users.noreply.github.com>
2022-10-08 19:40:01 -0400

    Update index.php

--- index.php
+++ index.php
@@ -14,16 +14,11 @@ echo "<br>";
 echo "php is working good";
 echo "<br>";
 
-$servername = "teambuilder.mysql.database.azure.com";
-$username = "Team10admin";
-$password = "hLGV3FisTb7P39z";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password);
-
-// Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL, "/home/site/certs/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_real_connect($conn, 'teambuilder.mysql.database.azure.com', 'Team10admin', 'hLGV3FisTb7P39z', 'teambuilder', 3306, MYSQLI_CLIENT_SSL);
+if (mysqli_connect_errno($conn)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 echo "Connected successfully";
 
