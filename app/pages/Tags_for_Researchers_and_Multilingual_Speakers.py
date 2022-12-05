a980e746f438ea8cc091810e981163bf50cad1e5
bostickt201 <bostickt201@gmail.com>
2022-12-04 23:49:49 -0500

    updated word cloud on advanced users page

--- app/pages/Tags_for_Researchers_and_Multilingual_Speakers.py
+++ app/pages/Tags_for_Researchers_and_Multilingual_Speakers.py
@@ -1,18 +1,20 @@
 import streamlit as st
 import json
 import requests
-from sample_if5 import top_5
+
+import streamlit as st
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
 from sample_if10 import top_10
 
-st.title("Looking for topic tags and nothing more? You've come to the right place.") # i.e., Nonnative Users
-#st.write("You have entered", st.session_state["my_input"])
+st.title("NLP researcher? Or just language savvy? Either way, add a little more flair to your topic tags.") # i.e., Multilingual/Research-oriented Users
 
 # Start of 'Instructions' section #################
 st.header("Instructions", anchor=None)
 
 st.text("Copy and paste your text in the space below.")
 st.text("Feel free to use as many or as few characters as you'd like.")
-
 # End of 'Instructions' section #################
 
 # input user text
@@ -29,65 +31,85 @@ placeholder=None,
 disabled=False,
 label_visibility="visible")
 
-# Top 5 button
-if st.button('Get Top 5 Best Tags'):
-    st.spinner(text="Please wait ... Retrieving Top 5 best tags ...")
+if st.button('Submit'):
+    st.spinner(text="Please wait ... Retrieving tags ...")
 
-    # Fetch top 5 best tags
+    # Fetch data and feed thru model
     try:
 
         # do something here with text
         input = {"input_text": text}
-
-        preds = top_5()
-        #res = requests.post(url = "https://a13c-34-143-172-49.ngrok.io/predict",
-    #          data = json.dumps(input))
-
-        # Output top 5 best tags below
-        st.text("Top 5 from prediction_hi_model_25:")
-        st.text(preds[0])
-
-        st.text("Top 5 from prediction_hi_model_50:")
-        st.text(preds[1])
-
-        st.text("Top 5 from prediction_en_model_25:")
-        st.text(preds[2])
-
-        st.text("Top 5 from prediction_en_model_50:")
-        st.text(preds[3])
-
-        #st.subheader(preds)
-
-    except ValueError:
-        st.text("Top 5 failed")
-
-
-# Top 10 button
-if st.button('Get Top 10 Best Tags'):
-    st.spinner(text="Please wait ... Retrieving Top 10 best tags ...")
-
-    # Fetch top 10 best tags
-    try:
-        # do something here with text
-        input = { "input_text": text}
 #        res = requests.post(url = "https://a13c-34-143-172-49.ngrok.io/predict",
 #              data = json.dumps(input))
 
-        preds = top_10()
-        # Output top 10 best tags below
 
-        # Output top 10 best tags below
+        # Start of 'Stats for Nerds' section
+        st.header("Stats for Nerds", anchor=None)
+
+        # Output Word Cloud visualization below
+
+        # get sample text
+        preds = top_10()
+
+        # Output top 10 best tags
         st.text("Top 10 from prediction_hi_model_25:")
         st.text(preds[0])
+
+        # Create and generate a word cloud image:
+        hi_25 = preds[0]
+        hi_25_str = ','.join(str(item) for item in hi_25)
+
+        #wordcloud = WordCloud().generate(hi_25_str)
+
+        #fig, ax = plt.subplots(figsize = (12, 8))
+        #ax.imshow(wordcloud)
+        #plt.axis("off")
+        #st.pyplot(fig)
+
+        # Output top 10 best tags
+        hi_50 = preds[1]
+        hi_50_str = ','.join(str(item) for item in hi_50)
 
         st.text("Top 10 from prediction_hi_model_50:")
         st.text(preds[1])
 
+        #wordcloud = WordCloud().generate(hi_50_str)
+
+        #fig, ax = plt.subplots(figsize = (12, 8))
+        #ax.imshow(wordcloud)
+        #plt.axis("off")
+        #st.pyplot(fig)
+
+        # Output top 10 best tags
         st.text("Top 10 from prediction_en_model_25:")
         st.text(preds[2])
+
+        en_25 = preds[2]
+        en_25_str = ','.join(str(item) for item in en_25)
+
+        wordcloud = WordCloud().generate(en_25_str)
+
+        fig, ax = plt.subplots(figsize = (12, 8))
+        ax.imshow(wordcloud)
+        plt.axis("off")
+        st.pyplot(fig)
+
+        # Output top 10 best tags
+        en_50 = preds[3]
+        en_50_str = ','.join(str(item) for item in en_50)
 
         st.text("Top 10 from prediction_en_model_50:")
         st.text(preds[3])
 
+        wordcloud = WordCloud().generate(en_50_str)
+
+        fig, ax = plt.subplots(figsize = (12, 8))
+        ax.imshow(wordcloud)
+        plt.axis("off")
+        st.pyplot(fig)
+
+        # Output request results below
+        #st.subheader(input)
+
     except ValueError:
-        st.text("Top 10 failed")
+        st.text("Submit failed")
