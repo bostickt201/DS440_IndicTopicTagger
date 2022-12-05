a5294c1cbcfc1eb6c02691035ea4b76f30f64169
bostickt201 <bostickt201@gmail.com>
2022-12-05 12:55:48 -0500

    update comments on home and pages

--- app/pages/Tags_for_Researchers_and_Multilingual_Speakers.py
+++ app/pages/Tags_for_Researchers_and_Multilingual_Speakers.py
@@ -11,10 +11,31 @@ from sample_if10 import top_10
 st.title("NLP researcher? Or just language savvy? Either way, add a little more flair to your topic tags.") # i.e., Multilingual/Research-oriented Users
 
 # Start of 'Instructions' section #################
-st.header("Instructions", anchor=None)
+st.subheader("Note # 1: Current functions include Top 10 best tags and word cloud for English-based models. These outputs are set to return for the sample output below:")
+st.text("Sample Case: ਗੁਜਰਾਤ ਦੇ 50 ਮਹਿਲਾ ਮੋਟਰਸਾਈਕਲ ਸਵਾਰਾਂ ਦੇ ਗਰੁੱਪ ‘ਬਾਈਕਿੰਗ ਕਵੀਨਜ਼’ ਨੇ ਅੱਜ ਇੱਥੇ ਪ੍ਰਧਾਨ ਮੰਤਰੀ, ਸ਼੍ਰੀ ਨਰੇਂਦਰ ਮੋਦੀ ਨਾਲ ਮੁਲਾਕਾਤ ਕੀਤੀ।ਗਰੁੱਪ ਦਾ ਕਹਿਣਾ ਹੈ ਕਿ ਉਨ੍ਹਾਂ ਨੇ 13 ਰਾਜਾਂ / ਕੇਂਦਰ ਸ਼ਾਸਤ ਪ੍ਰਦੇਸ਼ਾਂ ’ਚ 10,000 ਕਿਲੋਮੀਟਰ ਦਾ ਸਫਰ ਤੈਅ ਕਰਦਿਆਂ ਰਾਹ ਵਿੱਚ ਬੇਟੀ ਬਚਾਓ, ਬੇਟੀ ਪੜ੍ਹਾਓ ਅਤੇ ਸਵੱਛ ਭਾਰਤਵਰਗੇ ਕਈ ਸਮਾਜਿਕ ਮੁੱਦਿਆਂ ’ਤੇ ਲੋਕਾਂ ਨਾਲ ਚਰਚਾ ਕੀਤੀ।ਉਨ੍ਹਾਂ ਨੇ 15 ਅਗਸਤ 2017 ਨੂੰ ਲੱਦਾਖ ਦੇ ਖਰਦੁੰਗਲਾ ਵਿੱਖੇ ਤਿਰੰਗਾ ਲਹਿਰਾਇਆ।ਪ੍ਰਧਾਨ ਮੰਤਰੀ ਨੇ ਉਨ੍ਹਾਂ ਦੇ ਯਤਨਾਂ ਦੀ ਪ੍ਰਸ਼ੰਸਾ ਕੀਤੀ ਅਤੇ ਉਨ੍ਹਾਂ ਦੇ ਚੰਗੇ ਭਵਿੱਖ ਦੀ ਕਾਮਨਾ ਕੀਤੀ।"
+)
 
+st.subheader("Note # 2: Currently, only word cloud for English-based models and Top 10 tag functions exist in our 'Advanced Users' section. Future updates are set to include the below additional functions:")
+
+st.markdown("Topic Coherence score (NPMI)")
+st.markdown("Topic Similarity")
+st.markdown("Topic Matches")
+st.markdown("KL Divergence")
+st.markdown("Topic Quality Score by User")
+st.markdown("Topic Distribution Charts")
+
+st.markdown('''
+<style>
+[data-testid="stMarkdownContainer"] ul{
+    padding-left:80px;
+}
+</style>
+''', unsafe_allow_html=True)
+
+st.header("Instructions", anchor=None)
 st.text("Copy and paste your text in the space below.")
 st.text("Feel free to use as many or as few characters as you'd like.")
+
 # End of 'Instructions' section #################
 
 # input user text
@@ -59,26 +80,12 @@ if st.button('Submit'):
         hi_25 = preds[0]
         hi_25_str = ','.join(str(item) for item in hi_25)
 
-        #wordcloud = WordCloud().generate(hi_25_str)
-
-        #fig, ax = plt.subplots(figsize = (12, 8))
-        #ax.imshow(wordcloud)
-        #plt.axis("off")
-        #st.pyplot(fig)
-
         # Output top 10 best tags
         hi_50 = preds[1]
         hi_50_str = ','.join(str(item) for item in hi_50)
 
         st.text("Top 10 from prediction_hi_model_50:")
         st.text(preds[1])
-
-        #wordcloud = WordCloud().generate(hi_50_str)
-
-        #fig, ax = plt.subplots(figsize = (12, 8))
-        #ax.imshow(wordcloud)
-        #plt.axis("off")
-        #st.pyplot(fig)
 
         # Output top 10 best tags
         st.text("Top 10 from prediction_en_model_25:")
